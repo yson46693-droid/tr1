06b17ba34e261f742f44dd81574b6ac7b1dcc754
dev-osama74 <y648twm8sg@privaterelay.appleid.com>
2025-11-22 15:35:53 +0200

    Update - 2025-11-22 15:35

--- modules/manager/shipping_orders.php
+++ modules/manager/shipping_orders.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * إدارة طلبات شركات الشحن للمدير
- */
-
-if (!defined('ACCESS_ALLOWED')) {
-    die('Direct access not allowed');
-}
 
 require_once __DIR__ . '/../../includes/config.php';
 require_once __DIR__ . '/../../includes/db.php';
